3165515410992071aa6f290d1d22c3b38872b8de
Roman <markaryanroma@mail.ru>
2024-02-03 23:44:38 +0300

    Обновление composer.json Добавление code_sniffer Добавление psalm

--- app/Http/Controllers/MoneyController.php
+++ app/Http/Controllers/MoneyController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\ForexCost;
 use App\Service\Converter\Conversion;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\View\View;
 
 class MoneyController extends Controller
 {
-
-    public function mainPage() {
+    public function mainPage(): View
+    {
         $forexCost = json_decode(ForexCost::query()->latest()->first()['forex_cost_array'], true);
         /** @var array $forexCost */
         return view('main', data: [
@@ -22,7 +20,8 @@ class MoneyController extends Controller
         ]);
     }
 
-    public function getResult(Request $request, Conversion $conversion) {
+    public function getResult(Request $request, Conversion $conversion): View
+    {
         $requestData = $request->toArray();
         $forexCost = json_decode(ForexCost::query()->latest()->first()['forex_cost_array'], true);
         $result = $conversion->convertForex($requestData['from'], $requestData['to'], $requestData['count']);
@@ -30,5 +29,6 @@ class MoneyController extends Controller
             "title" => 'Конвертер валют',
             "currencies" => $forexCost,
             "result" => $result,
-        ]);    }
+        ]);
+    }
 }
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
+    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
 );
